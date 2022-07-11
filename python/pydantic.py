4413f39fd4b94b11f33bfdb46c31dc453a9ceaa5
r0drig <andralmeida0@gmail.com>
2022-07-11 23:37:15 +0100

    Pydantic cheat sheet

---
+++ python/pydantic.py
@@ -0,0 +1,7 @@
+#https://towardsdatascience.com/8-reasons-to-start-using-pydantic-to-improve-data-parsing-and-validation-4f437eae7678
+
+#https://pydantic-docs.helpmanual.io/usage/models/
+
+#https://towardsdatascience.com/9-reasons-why-you-should-start-using-python-dataclasses-98271adadc66
+
+#https://towardsdatascience.com/pydantic-or-dataclasses-why-not-both-convert-between-them-ba382f0f9a9c
